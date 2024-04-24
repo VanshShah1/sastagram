5d071bf2677d2b0fa72c6c673e8eb7abb2924c93
Vansh Shah <75292413+VanshShah1@users.noreply.github.com>
2024-04-24 13:31:27 +0530

    sndfjksnfsf

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.title="sastagram"
+    page.bgcolor="black"
+    page.navigation_bar=ft.CupertinoNavigationBar(destinations=[ft.NavigationDestination(icon=ft.icons.HOME, label="Home"),
+                                                                ft.NavigationDestination(icon=ft.icons.ADD, label="Post"),
+                                                                ft.NavigationDestination(icon=ft.icons.PERSON, label="Profile")],
+                                                                bgcolor=ft.colors.TRANSPARENT,
+                                                                border=ft.border.Border(top=ft.BorderSide(1, ft.colors.GREY_900), bottom=ft.BorderSide(0, ft.colors.GREY_900), left=ft.BorderSide(0, ft.colors.GREY_900), right=ft.BorderSide(0, ft.colors.GREY_900)),
+                                                                inactive_color="grey",
+                                                                active_color="white"
+                                                                )
 
+
+    page.add()
 
 ft.app(main)
